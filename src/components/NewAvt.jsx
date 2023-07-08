e8253998bea40ac02890e09583bd38bf61e8ba2a
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-07-07 20:37:33 -0500

    Render the 3d model on the screen

---
+++ src/components/NewAvt.jsx
@@ -0,0 +1,85 @@
+// Avatar component renders a 3D avatar model with animations.
+import React from "react";
+import { useFBX, useGLTF } from "@react-three/drei";
+
+export function Avatar(props) {
+  const { nodes, materials } = useGLTF("models/newAvt.glb");
+
+  // Load animations
+  const { animations: typingAnimation } = useFBX("animations/Typing.fbx");
+
+  // console.log(Me typing) and Customize animations
+  typingAnimation[0].name = "AcklesTyping";
+  typingAnimation.map((animation, index) => {
+    console.log(animation);
+  });
+  // Render the 3D model
+  return (
+    <group {...props} dispose={null}>
+      <primitive object={nodes.Hips} />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Body.geometry}
+        material={materials.Wolf3D_Body}
+        skeleton={nodes.Wolf3D_Body.skeleton}
+      />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+        material={materials.Wolf3D_Outfit_Bottom}
+        skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+      />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+        material={materials.Wolf3D_Outfit_Footwear}
+        skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+      />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Outfit_Top.geometry}
+        material={materials.Wolf3D_Outfit_Top}
+        skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+      />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Hair.geometry}
+        material={materials.Wolf3D_Hair}
+        skeleton={nodes.Wolf3D_Hair.skeleton}
+      />
+      <skinnedMesh
+        name='EyeLeft'
+        geometry={nodes.EyeLeft.geometry}
+        material={materials.Wolf3D_Eye}
+        skeleton={nodes.EyeLeft.skeleton}
+        morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
+        morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+      />
+      <skinnedMesh
+        name='EyeRight'
+        geometry={nodes.EyeRight.geometry}
+        material={materials.Wolf3D_Eye}
+        skeleton={nodes.EyeRight.skeleton}
+        morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
+        morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+      />
+      <skinnedMesh
+        name='Wolf3D_Head'
+        geometry={nodes.Wolf3D_Head.geometry}
+        material={materials.Wolf3D_Skin}
+        skeleton={nodes.Wolf3D_Head.skeleton}
+        morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+        morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+      />
+      <skinnedMesh
+        name='Wolf3D_Teeth'
+        geometry={nodes.Wolf3D_Teeth.geometry}
+        material={materials.Wolf3D_Teeth}
+        skeleton={nodes.Wolf3D_Teeth.skeleton}
+        morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+        morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+      />
+    </group>
+  );
+}
+// Preloading the 3D model file helps improve the performance of the application because it allows 
+//the model to be loaded and cached in the browser's memory ahead of time. This way,
+// when the component using the model is rendered, the model can be retrieved quickly from the cache
+// instead of having to wait for it to be loaded from the network.This can result in smoother and
+// faster rendering of the 3D model in the application.
+useGLTF.preload("models/newAvt.glb");
